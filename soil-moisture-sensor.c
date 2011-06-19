7c14ed253a2be92e28e7b5c971969ae8cff820fb
Robert Quattlebaum <darco@deepdarc.com>
2011-06-18 19:03:55 -0700

    Source code cleanup for new one-zone sensor.

--- soil-moisture-sensor.c
+++ soil-moisture-sensor.c
@@ -1,7 +1,4 @@
 
-
-#define USE_DRAIN_METHOD 0
-
 #include <stdint.h>
 #include <stdbool.h>
 #include <avr/io.h>
@@ -11,76 +8,46 @@
 #define sbi(x,y)	x|=(1<<y)
 #define cbi(x,y)	x&=~(1<<y)
 
-#if FULLY_DRIVE_PULSES
-#define ZONE_A_PULSE()		sbi(PORTB,3),sbi(DDRB,3),_delay_us(100),cbi(DDRB,3),cbi(PORTB,3)
-#define ZONE_B_PULSE()		sbi(PORTB,4),sbi(DDRB,4),_delay_us(100),cbi(DDRB,4),cbi(PORTB,4)
-#else
-#define ZONE_A_PULSE()		sbi(PORTB,3),cbi(PORTB,3)
-#define ZONE_B_PULSE()		sbi(PORTB,4),cbi(PORTB,4)
-#endif
+#define MOIST_FULLY_DRIVE_PULSES	1
 
-#define COLLECTOR_CHECK()	(PINB&(1<<2))
+#define MOIST_DRIVE_PIN		1
+#define MOIST_COLLECTOR_PIN	3
+#define MOIST_MAX_VALUE		((1l<<(sizeof(moist_value)*8l))-1)
 
-uint8_t zone_a;
-uint8_t zone_a_floor = 0xFF;
-uint8_t zone_a_ceiling;
+uint8_t moist_value = 0x00;
+uint8_t moist_floor = 0xFF;
+uint8_t moist_ceiling = 0x00;
 
-uint8_t zone_b;
-uint8_t zone_b_floor = 0xFF;
-uint8_t zone_b_ceiling;
-
-void reset() {
-	PORTB = 0x00;
-	DDRB = (1<<3)|(1<<4)|(1<<2);
+void
+moist_update() {
+	// Make sure pins are configured.
+	cbi(PORTB,MOIST_DRIVE_PIN);
+	cbi(PORTB,MOIST_COLLECTOR_PIN);
+	sbi(DDRB,MOIST_DRIVE_PIN);
+	sbi(DDRB,MOIST_COLLECTOR_PIN);
 	_delay_ms(50);
-	DDRB = 0x00;
-}
+	cbi(DDRB,MOIST_DRIVE_PIN);
+	cbi(DDRB,MOIST_COLLECTOR_PIN);
 
-
-void zone_a_moisture() {
-	uint8_t i;
-	zone_a = 0;
-	for(i=0;i<1;i++) {
-		reset();
-#if USE_DRAIN_METHOD
-		for(sbi(PORTB,3);!(PINB&(1<<3));zone_a++);
-#else
-		for(;(zone_a!=((1l<<(sizeof(zone_a)*8l))-1)) && !COLLECTOR_CHECK();zone_a++) {
-			ZONE_A_PULSE();
-			_delay_us(1);
-		}
+	for(moist_value=0;
+		(moist_value!=MOIST_MAX_VALUE) && !(PINB&(1<<MOIST_COLLECTOR_PIN));
+		moist_value++
+	) {
+		sbi(PORTB,MOIST_DRIVE_PIN);
+#if MOIST_FULLY_DRIVE_PULSES
+		sbi(DDRB,MOIST_DRIVE_PIN);
+		cbi(DDRB,MOIST_DRIVE_PIN);
 #endif
+		cbi(PORTB,MOIST_DRIVE_PIN);
+
+		_delay_us(1);
 	}
-	if(zone_a<zone_a_floor)
-		zone_a_floor = zone_a;
 
-	zone_a -= zone_a_floor;
+	if(moist_value<moist_floor)
+		moist_floor = moist_value;
 
-	if(zone_a>zone_a_ceiling)
-		zone_a_ceiling = zone_a;
-}
-
-void zone_b_moisture() {
-	uint8_t i;
-	zone_b = 0;
-	for(i=0;i<1;i++) {
-		reset();
-#if USE_DRAIN_METHOD
-		for(sbi(PORTB,4);!(PINB&(1<<4));zone_b++);
-#else
-		for(;(zone_b!=((1l<<(sizeof(zone_b)*8l))-1)) && !COLLECTOR_CHECK();zone_b++) {
-			ZONE_B_PULSE();
-			_delay_us(1);
-		}
-#endif
-	}
-	if(zone_b<zone_b_floor)
-		zone_b_floor = zone_b;
-	
-	zone_b -= zone_b_floor;
-
-	if(zone_b>zone_b_ceiling)
-		zone_b_ceiling = zone_b;
+	if(moist_value>moist_ceiling)
+		moist_ceiling = moist_value;
 }
 
 /*
@@ -112,21 +79,10 @@ one_wire_slave() {
 	sbi(GIMV,PCIE);
 
 }
-
 */
 
-#include "xmas.h"
-
-
-
-
 int main(void) {
-/*
-PB2 = collector
-PB3 = zone a
-PB4 = zone b */
 	while(1) {
-		zone_a_moisture();
-		zone_b_moisture();
+		moist_update();
 	}
 }
